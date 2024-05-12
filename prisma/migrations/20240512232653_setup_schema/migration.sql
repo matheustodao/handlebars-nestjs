855fc386a30ec85cf1fc2633f6f8baba931c5776
Matheus Todao <itodaomatheus@gmail.com>
2024-05-12 20:29:58 -0300

    feat: add migration

---
+++ prisma/migrations/20240512232653_setup_schema/migration.sql
@@ -0,0 +1,32 @@
+-- CreateEnum
+CREATE TYPE "tasks_status" AS ENUM ('PENDING', 'DONE');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "tasks" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "status" "tasks_status" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "tasks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_id_key" ON "users"("id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "tasks_id_key" ON "tasks"("id");
